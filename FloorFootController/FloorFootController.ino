1dc0dedec0391e1df557f8108fd2d8eb97b73184
laps78 <9254384@mail.ru>
2022-05-10 23:17:51 +0300

    midi foot controller ins first initialization added

--- FloorFootController/FloorFootController.ino
+++ FloorFootController/FloorFootController.ino
@@ -1,5 +1,34 @@
-void setup()
+//initialize variables & pins
+#define button1 1//button 1
+#define button2 2//button 1
+#define button3 3//button 1
+#define button4 4//button 1
+#define button5 5//button 1
+#define button6 6//button 1
+#define button7 7//button 1
+#define button8 8//button 1
+#define buttonLoopStop 9//button 1
+#define buttonLoopDel 10//button 1
+#define buttonLoopMain 11//button 1
+#define buttonSelect 12//button 1
 
 
+void setup() {
 
-void loop()
+pinMode(button1, INPUT);
+pinMode(button2, INPUT);
+pinMode(button2, INPUT);
+pinMode(button3, INPUT);
+pinMode(button4, INPUT);
+pinMode(button5, INPUT);
+pinMode(button6, INPUT);
+pinMode(button7, INPUT);
+pinMode(button8, INPUT);
+pinMode(buttonLoopDel, INPUT);
+pinMode(buttonSelect, INPUT);
+pinMode(buttonLoopStop, INPUT);
+pinMode(buttonLoopMain, INPUT);
+}
+
+void loop(){
+}
